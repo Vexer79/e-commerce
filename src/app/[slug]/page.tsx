fe783ad33c9cf997b2dca69d492595862ec74c58
Vexer79 <rokytskyi.i@gmail.com>
2024-08-15 00:24:42 +0200

    feat: add fetching single product

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,55 +1,66 @@
+import { wixClientServer } from "@/lib/wixClientServer";
 import Add from "../../components/Add";
 import CustomizeProducts from "../../components/CustomizeProducts";
 import ProductImages from "../../components/ProductImages";
+import { notFound } from "next/navigation";
+import DOMPurify from "isomorphic-dompurify";
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+    const wixClient = await wixClientServer();
+    console.log(params.slug);
+
+    const products = await wixClient.products.queryProducts().eq("slug", params.slug).find();
+    if (!products.items[0]) {
+        return notFound();
+    }
+    const product = products.items[0];
     return (
         <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
             {/* IMG */}
             <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-                <ProductImages />
+                <ProductImages items={product.media?.items} />
             </div>
             {/* TEXTS */}
             <div className="w-full lg:w-1/2 flex flex-col gap-6">
-                <h1 className="text-4xl font-medium">Product Name</h1>
-                <p className="text-gray-500">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui ex aliquid quasi
-                    ab voluptate cum repudiandae placeat non voluptas nobis dolor nesciunt porro
-                    quisquam, recusandae voluptatem consectetur, sint aperiam delectus.
-                </p>
+                <h1 className="text-4xl font-medium">{product.name}</h1>
+                <p
+                    className="text-gray-500"
+                    dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(product.description || ""),
+                    }}
+                ></p>
                 <div className="h-[2px] bg-gray-100" />
-                <div className="">
-                    <h3 className="text-xl text-gray-500 line-through">$59</h3>
-                    <h2 className="font-medium text-2xl">$49</h2>
-                </div>
+                {product.priceData?.discountedPrice === product.priceData?.price ? (
+                    <h3 className="text-xl text-gray-500 line-through">
+                        {product.priceData?.price}
+                        {product.priceData?.currency}
+                    </h3>
+                ) : (
+                    <div className="flex items-center gap-4">
+                        <h3 className="text-xl text-gray-500 line-through">
+                            {product.priceData?.price}
+                            {product.priceData?.currency}
+                        </h3>
+                        <h2 className="font-medium text-2xl">
+                            {product.priceData?.discountedPrice}
+                            {product.priceData?.currency}
+                        </h2>
+                    </div>
+                )}
                 <div className="h-[2px] bg-gray-100" />
                 <CustomizeProducts />
                 <Add />
                 <div className="h-[2px] bg-gray-100" />
-                <div className="text-sm">
-                    <h4 className="font-medium mb-4">Title</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab dolorem
-                        labore, voluptatibus aliquid voluptates natus eveniet nostrum necessitatibus
-                        voluptas totam aut dolore itaque voluptatem ad, iure quos vitae asperiores?
-                    </p>
-                </div>
-                <div className="text-sm">
-                    <h4 className="font-medium mb-4">Title</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab dolorem
-                        labore, voluptatibus aliquid voluptates natus eveniet nostrum necessitatibus
-                        voluptas totam aut dolore itaque voluptatem ad, iure quos vitae asperiores?
-                    </p>
-                </div>
-                <div className="text-sm">
-                    <h4 className="font-medium mb-4">Title</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab dolorem
-                        labore, voluptatibus aliquid voluptates natus eveniet nostrum necessitatibus
-                        voluptas totam aut dolore itaque voluptatem ad, iure quos vitae asperiores?
-                    </p>
-                </div>
+                {product.additionalInfoSections?.map((section: any) => (
+                    <div className="text-sm" key={section.title}>
+                        <h4 className="font-medium mb-4">{section.title}</h4>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(section.description || ""),
+                            }}
+                        ></p>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -2,32 +2,14 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-    {
-        id: 0,
-        imgUrl: "https://images.pexels.com/photos/27215761/pexels-photo-27215761.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-    },
-    {
-        id: 1,
-        imgUrl: "https://images.pexels.com/photos/13760155/pexels-photo-13760155.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    },
-    {
-        id: 2,
-        imgUrl: "https://images.pexels.com/photos/14452691/pexels-photo-14452691.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    },
-    {
-        id: 3,
-        imgUrl: "https://images.pexels.com/photos/8155559/pexels-photo-8155559.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    },
-];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
     const [index, setIndex] = useState(0);
     return (
         <div className="">
             <div className="h-[500px] relative">
                 <Image
-                    src={images[index].imgUrl}
+                    src={items[index].image?.url}
                     alt=""
                     fill
                     sizes="50vw"
@@ -35,14 +17,14 @@ const ProductImages = () => {
                 />
             </div>
             <div className="flex justify-evenly gap-4 mt-8">
-                {images.map((image, index) => (
+                {items.map((item: any, index: number) => (
                     <div
                         className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-                        key={image.id}
+                        key={item._id}
                         onClick={() => setIndex(index)}
                     >
                         <Image
-                            src={image.imgUrl}
+                            src={item.image?.url}
                             alt=""
                             fill
                             sizes="30vw"
