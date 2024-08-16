a081d4787803dcaa1ab200c99756432d6d590330
Vexer79 <rokytskyi.i@gmail.com>
2024-08-16 15:26:54 +0200

    feat: add orders page

--- src/hooks/useCartStore.tsx
+++ src/hooks/useCartStore.tsx
@@ -16,12 +16,17 @@ export const useCartStore = create<CartState>((set) => ({
     isLoading: false,
     counter: 0,
     getCart: async (wixClient) => {
-        const cart = await wixClient.currentCart.getCurrentCart();
-        set({
-            cart: cart || [],
-            isLoading: false,
-            counter: cart?.lineItems.length || 0,
-        });
+        try {
+            const cart = await wixClient.currentCart.getCurrentCart();
+            set({
+                cart: cart || [],
+                isLoading: false,
+                counter: cart?.lineItems.length || 0,
+            });
+        } catch (err) {
+            console.log(err);
+            set((prev) => ({ ...prev, isLoading: false }));
+        }
     },
     addItem: async (wixClient, productId, variantId, quantity) => {
         set((state) => ({ ...state, isLoading: true }));
